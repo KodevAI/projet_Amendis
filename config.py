91fd91a3d1d0977de86338936de5e1475d481fa8
KodevAI <kodevai.contact@gmail.com>
2025-06-30 16:41:44 +0100

    commit

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
-class Config:
-    # Remplace les éléments de l'URI par tes propres informations
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:12345@localhost/amendis'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'secretkey'
-# import os
-
 # class Config:
-#     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@db:3306/amendis'
+#     # Remplace les éléments de l'URI par tes propres informations
+#     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:12345@localhost/amendis'
 #     SQLALCHEMY_TRACK_MODIFICATIONS = False
-#     SECRET_KEY = os.getenv('SECRET_KEY', 'secretkey')
+#     SECRET_KEY = 'secretkey'
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@db:3306/amendis'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('SECRET_KEY', 'secretkey')
